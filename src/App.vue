c5ba44ecf67c4a5f3329a7597ada7ab09a4bf7e7
youpoko0404 <clan.sf123@gmail.com>
2020-08-21 15:06:14 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
   <div id="app">
     <fight msg />
     <div class="row">
-      <div class="col-3">
-      </div>
+      <div class="col-3"></div>
     </div>
   </div>
 </body>
@@ -27,13 +26,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 body {
-  background-image: url(./assets/img/bg.jpg);
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  background-color: white;
 }
 </style>
--- src/components/fight.vue
+++ src/components/fight.vue
@@ -1,47 +1,127 @@
 <template>
   <div>
-    <button @click="randomA()" class="btn btn-warning">START {{ i }}</button>
-    <button @click="randomDamage(10,100)" class="btn btn-warning">Random {{ randomNo }}</button>
+    <button @click="randomStart()" class="btn btn-danger">Start</button>
+    <div class="row">
+      <div class="col-12">
+        <button @click="randomDamage(0,50)" class="btn btn-info">Attack</button>
+        <button @click="randomDamageSP(50,100)" class="btn btn-info">Special Attack</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-6">
+        <p>{{randomPlayer}}</p>
+        <p>HP : {{hp1}}</p>
+        <img :src="image1" />
+      </div>
+
+      <div class="col-6">
+        <p>{{randomMonster}}</p>
+        <p>HP : {{hp2}}</p>
+        <img :src="image2" />
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
   data: function () {
     return {
-      counter: 0,
-      fsize: 50,
-      randomNo: 0,
-      i: null,
+      randomAttack: "",
+      randomSpAttack: "",
+      image1: "",
+      image2: "",
+      hp1: 0,
+      hp2: 0,
+
       player: [
-        { name: "I", hp: 100, image: "" },
-        { name: "D", hp: 100, image: "" },
-        { name: "A", hp: 100, image: "" },
+        {
+          name: "Batman",
+          hp: 300,
+          image: "./assets/img/batman.png",
+        },
+        {
+          name: "Joker",
+          hp: 200,
+          image: "./assets/img/joker.png",
+        },
+        {
+          name: "Superman",
+          hp: 400,
+          image: "./assets/img/superman.png",
+        },
       ],
-      Monster: [
-        { name: "T", hp: 100, image: "" },
-        { name: "C", hp: 100, image: "" },
-        { name: "P", hp: 100, image: "" },
+      randomPlayer: "",
+      monster: [
+        {
+          name: "Hukl",
+          hp: 350,
+          image: "./assets/img/hulk.png",
+        },
+        {
+          name: "Ironman",
+          hp: 240,
+          image: "./assets/img/ironman.png",
+        },
+        {
+          name: "Sipderman",
+          hp: 230,
+          image: "./assets/img/sipderman.png",
+        },
       ],
+      randomMonster: "",
     };
   },
+
   props: {
-    lable: String,
+    Label: String,
   },
+
   methods: {
-    randomFI: function (min, max) {
-      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    randomStart: function () {
+      var chosenNumber1 = Math.floor(Math.random() * this.player.length);
+      this.randomPlayer = this.player[chosenNumber1].name;
+      this.hp1 = this.player[chosenNumber1].hp;
+      this.image1 = this.player[chosenNumber1].image;
+
+      var chosenNumber2 = Math.floor(Math.random() * this.monster.length);
+      this.randomMonster = this.monster[chosenNumber2].name;
+      this.hp2 = this.monster[chosenNumber2].hp;
+      this.image2 = this.monster[chosenNumber2].image;
     },
-    randomA: function () {
-      console.log("caLL FUNCTION RAN");
-      var x=this.randomFI(0,2);
-      console.log('x= '+x);
-      this.i = this.player[x];
+
+    randomDamage: function (min, max) {
+      this.randomAttack = Math.max(Math.floor(Math.random() * max) + 1, min);
+      console.log(this.randomAttack);
+      console.log(this.hp2);
+      if (this.hp1 != 0 && this.hp2 != 0) {
+        this.hp2 -= this.randomAttack;
+      }
+      if (this.hp2 != 0 && this.hp1 != 0) {
+        this.hp1 -= this.randomAttack;
+      }
+      if (this.hp1 <= 0) {
+        this.hp1 = 0;
+        console.log("LOSE");
+      }
+      if (this.hp2 <= 0) {
+        this.hp2 = 0;
+        console.log("WIN");
+      }
     },
-     randomDamage: function (min, max) {
-      this.randomNo =  Math.max(Math.floor(Math.random() * max) + 1, min);
+
+    randomDamageSP: function (min, max) {
+      this.randomAttack = Math.max(Math.floor(Math.random() * max) + 1, min);
+      console.log(this.randomAttack);
+      this.hp2 -= this.randomAttack;
+      if (this.hp2 <= 0) {
+        this.hp2 = 0;
+      }
     },
   },
 };
 </script>
+
 <style>
 </style>
